cad67779373c3748aa6c74c491382da34056f8bb
Kaelan Young <86504531+KaelanY@users.noreply.github.com>
2026-05-30 18:51:35 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Arrays for dynamic content
 const quotes = [
-    "Make it simple, but significant.",
-    "Complexity is your enemy. Any fool can make something complicated.",
-    "Simplicity is the ultimate sophistication.",
-    "Action is the foundational key to all success.",
-    "Code is like humor. When you have to explain it, it’s bad."
+    "I AM SIGMA",
+    "Rome was not built in a day",
+    "Those",
+    "🗣️💯💯🙏🙏",
+    "The only impossible journey is the one you never begin"
 ];
 
 const colors = ["#f0f2f5", "#e3faf2", "#fff3e6", "#ede7f6", "#e8f0fe"];
